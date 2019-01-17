50bd5e38b014917f18f2ae6f69a1b4e18e2eba87
Aidan Steele <aidan.steele@glassechidna.com.au>
2019-01-18 09:40:07 +1100

    Brew formula update for dynamo version 0.3.0

--- dynamo.rb
+++ dynamo.rb
@@ -1,9 +1,9 @@
 class Dynamo < Formula
   desc "dynamo is a dead-simple CLI for AWS DynamoDB"
   homepage "https://github.com/glassechidna/dynamo"
-  url "https://github.com/glassechidna/dynamo/releases/download/0.2.0/dynamo_0.2.0_Darwin_x86_64.tar.gz"
-  version "0.2.0"
-  sha256 "23f3b3776f98086a7efed1c7709ef05597ff39171131ff7cf030491c579ef644"
+  url "https://github.com/glassechidna/dynamo/releases/download/0.3.0/dynamo_0.3.0_Darwin_x86_64.tar.gz"
+  version "0.3.0"
+  sha256 "064f786c0c88c6174e67acd8c55f010aca749290905ca6fce031a4b99a505360"
 
   def install
     bin.install "dynamo"
