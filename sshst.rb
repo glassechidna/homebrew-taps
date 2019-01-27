02a8f6b3cf62cbc83d92da8b56d0a193e5b7d5ed
Aidan Steele <aidan.steele@glassechidna.com.au>
2019-01-27 18:40:00 +1100

    Brew formula update for sshst version v0.1.13

--- sshst.rb
+++ sshst.rb
@@ -1,9 +1,9 @@
 class Sshst < Formula
   desc "TODO"
   homepage "https://ssh.st"
-  url "http://downloads-ap-southeast-2.ssh.st/brew/sshst_0.1.12_Darwin_x86_64.tar.gz", :using => CurlDownloadStrategy
-  version "0.1.12"
-  sha256 "bcc599876b893b259e6ab69481afa00ba064d1e18c0adafd515dde35104d9e89"
+  url "http://downloads-ap-southeast-2.ssh.st/brew/sshst_0.1.13_Darwin_x86_64.tar.gz", :using => CurlDownloadStrategy
+  version "0.1.13"
+  sha256 "bbdcb9d1d744f5ba1bd8744912341e0d8234951e22808f41480a1661062b7208"
 
   def install
     bin.install "sshst"
