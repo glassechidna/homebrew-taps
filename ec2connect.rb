c25975cf14d5625e2f3fe2638d175a12d5f5719e
Aidan Steele <aidan.steele@glassechidna.com.au>
2019-06-28 21:16:57 +1000

    Brew formula update for ec2connect version 0.0.2

--- ec2connect.rb
+++ ec2connect.rb
@@ -2,9 +2,9 @@
 class Ec2connect < Formula
   desc "ec2connect is a convenient SSH wrapper around EC2 instance connect"
   homepage "https://github.com/glassechidna/ec2connect"
-  url "https://github.com/glassechidna/ec2connect/releases/download/0.0.1/ec2connect_0.0.1_Darwin_x86_64.tar.gz"
-  version "0.0.1"
-  sha256 "e8b724017dced87c886ee625a5c5c138c69116c76ea271be66927353e7fd900f"
+  url "https://github.com/glassechidna/ec2connect/releases/download/0.0.2/ec2connect_0.0.2_Darwin_x86_64.tar.gz"
+  version "0.0.2"
+  sha256 "29238df5e435a1b2179eb152cf82b872fa974d7aa2d52c8d945c42a1bf358935"
 
   def install
     bin.install "ec2connect"
