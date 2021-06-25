9d1436acd4b6208b17d078b8a486421dc2c0ea7f
Aidan Steele <aidan.steele@glassechidna.com.au>
2021-06-25 11:03:12 +1000

    Brew formula update for efsu version 0.3.0

--- efsu.rb
+++ efsu.rb
@@ -5,28 +5,28 @@
 class Efsu < Formula
   desc "efsu is for accessing AWS EFS from your machine without a VPN"
   homepage "https://github.com/glassechidna/efsu"
-  version "0.2.1"
+  version "0.3.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/glassechidna/efsu/releases/download/0.2.1/efsu_0.2.1_darwin_amd64.tar.gz"
-      sha256 "7750cd38da3cc2799491aa39d33a07e421b068e4e9a794796d00d70cb3710869"
+      url "https://github.com/glassechidna/efsu/releases/download/0.3.0/efsu_0.3.0_darwin_amd64.tar.gz"
+      sha256 "d4d79138408700a92c976a0bc6df20f7b46ec93a246063914109d3f63465ee55"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/glassechidna/efsu/releases/download/0.2.1/efsu_0.2.1_darwin_arm64.tar.gz"
-      sha256 "61a2fca2525a8d1bbab4ddcdf34b10bf6428de90f8bb39ea0b8e28d08381f458"
+      url "https://github.com/glassechidna/efsu/releases/download/0.3.0/efsu_0.3.0_darwin_arm64.tar.gz"
+      sha256 "865dca3674a7211fcd02ac569975dda99abc8517a7b662a90137d2facdfd4cba"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/glassechidna/efsu/releases/download/0.2.1/efsu_0.2.1_linux_amd64.tar.gz"
-      sha256 "bd5b21a13106f16ff9b4e4290e6b65e32371df94f08631565389e5c7afd5317c"
+      url "https://github.com/glassechidna/efsu/releases/download/0.3.0/efsu_0.3.0_linux_amd64.tar.gz"
+      sha256 "bbdb257f34a4fce6859766a8dac0e263722a9e2fa5a1aad85b2948ecf48ff0da"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/glassechidna/efsu/releases/download/0.2.1/efsu_0.2.1_linux_arm64.tar.gz"
-      sha256 "56fe9f94f1c074fe23e1615d3a0b8309e91d2f22ba61d3d5efc2980ba6e47a9e"
+      url "https://github.com/glassechidna/efsu/releases/download/0.3.0/efsu_0.3.0_linux_arm64.tar.gz"
+      sha256 "f4c768506542017cf24c4eecc86d290b417705e0da125ffcf3e2d3f3697dbbea"
     end
   end
 
